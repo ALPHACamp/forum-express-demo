6c1f3c35e89a7146bba6813949e9c407a9a06a8f
Ellen Lee <leechinghui.tw@gmail.com>
2020-11-05 12:12:14 +0800

    Create user model

---
+++ models/user.js
@@ -0,0 +1,39 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  };
+  User.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User'
+  })
+  return User
+}
+
+// 以下是使用 sequelize.define 的版本
+// 'use strict'
+// module.exports = (sequelize, DataTypes) => {
+//   const User = sequelize.define('User', {
+//     name: DataTypes.STRING,
+//     email: DataTypes.STRING,
+//     password: DataTypes.STRING
+//   }, {})
+//   User.associate = function (models) {
+//     // associations can be defined here
+//   }
+//   return User
+// }
