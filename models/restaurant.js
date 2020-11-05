638dc8e049d1fb3d8dc69aa54f8eed1563e0b2f8
Ellen Lee <leechinghui.tw@gmail.com>
2020-11-05 12:24:16 +0800

    Create restaurant model

---
+++ models/restaurant.js
@@ -0,0 +1,43 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Restaurant extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  };
+  Restaurant.init({
+    name: DataTypes.STRING,
+    tel: DataTypes.STRING,
+    address: DataTypes.STRING,
+    opening_hours: DataTypes.STRING,
+    description: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Restaurant'
+  })
+  return Restaurant
+}
+
+// 以下是使用 sequelize.define 的版本
+// 'use strict';
+// module.exports = (sequelize, DataTypes) => {
+//   const Restaurant = sequelize.define('Restaurant', {
+//     name: DataTypes.STRING,
+//     tel: DataTypes.STRING,
+//     address: DataTypes.STRING,
+//     opening_hours: DataTypes.STRING,
+//     description: DataTypes.TEXT
+//   }, {});
+//   Restaurant.associate = function (models) {
+//     // associations can be defined here
+//   };
+//   return Restaurant;
+// };
