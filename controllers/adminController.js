662ce0c9173d9940f1ae9fc133d0a121fafb6165
Ellen Lee <leechinghui.tw@gmail.com>
2020-11-06 17:59:19 +0800

    Show category name on admin/restaurants

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,10 +4,16 @@ const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const db = require('../models')
 const Restaurant = db.Restaurant
 const User = db.User
+const Category = db.Category
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ raw: true }).then(restaurants => {
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    }).then(restaurants => {
+      // console.log(restaurants) // 加入 console 觀察資料的變化
       return res.render('admin/restaurants', { restaurants: restaurants })
     })
   },
