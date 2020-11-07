c8db783341a5b47ca20e5d2f7a975ef9dea767a6
Ellen Lee <leechinghui.tw@gmail.com>
2020-11-07 20:48:10 +0800

    [Assignment] Add dashboard page

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,9 @@ module.exports = (app, passport) => {
   app.get('/restaurants', authenticated, restController.getRestaurants)
   app.get('/restaurants/feeds', authenticated, restController.getFeeds)
   app.get('/restaurants/:id', authenticated, restController.getRestaurant)
+  app.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
+
+
   app.post('/comments', authenticated, commentController.postComment)
   app.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 
