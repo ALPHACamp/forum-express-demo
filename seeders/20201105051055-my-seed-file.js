87f4b6484f65428e8abb22640a899b6d3a9e9e73
v123582 <v123582@gmail.com>
2020-11-08 23:37:11 +0800

    fix Cannot add or update a child row: a foreign key constraint fails

--- seeders/20201105051055-my-seed-file.js
+++ seeders/20201105051055-my-seed-file.js
@@ -66,7 +66,7 @@ module.exports = {
           id: i * 10 + 1,
           text: faker.lorem.sentence(),
           UserId: Math.floor(Math.random() * 3) * 10 + 1,
-          RestaurantId: i % 50 + 1,
+          RestaurantId: i % 50 * 10 + 1,
           createdAt: new Date(),
           updatedAt: new Date()
         })
